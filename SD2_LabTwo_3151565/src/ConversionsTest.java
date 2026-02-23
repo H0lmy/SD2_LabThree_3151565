9c241ca15e1c09be361ba8f0286d3706cb4adb93
Maksym Shtymak <maksym.shtymak@student.griffith.ie>
2026-02-23 15:56:32 +0200

    Add conversions stub methods

--- SD2_LabTwo_3151565/src/ConversionsTest.java
+++ SD2_LabTwo_3151565/src/ConversionsTest.java
@@ -1,2 +1,32 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
 public class ConversionsTest {
+    private static Conversions c;
+    @BeforeAll
+    public static void setup() {
+        c = new Conversions();
+    }
+
+
+    @Test
+    public void euroToDollarDefaultValue() {
+        double result = c.euroToDollar(100);
+        assert(result==110);
+    }
+
+    @Test
+    public void euroToDollarNegativeValue() {
+        double result = c.euroToDollar(-100);
+        assert(result==0);
+    }
+
+    @Test
+    public void euroToDollarZeroValue() {
+        double result = c.euroToDollar(0);
+        assert(result==0);
+    }
+
+
 }
