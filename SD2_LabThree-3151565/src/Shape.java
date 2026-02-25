fe1e9dda77ac9631c15a2dc2956e868b30869f3c
Maksym Shtymak <maksym.shtymak@student.griffith.ie>
2026-02-25 15:30:28 +0200

    add Shape abstract class

---
+++ SD2_LabThree-3151565/src/Shape.java
@@ -0,0 +1,22 @@
+public  abstract class Shape {
+    private String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    public String toString() {
+        return "Shape: " + name;
+    }
+}
