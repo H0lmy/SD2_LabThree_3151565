3caa3d761454ff7c93262a8b97629cfd7b7ed3e4
Maksym Shtymak <maksym.shtymak@student.griffith.ie>
2026-02-26 15:20:00 +0200

    create an integration testing TestShapeList file which test the getArea and getPerimeter methods in the shapes list

---
+++ SD2_LabThree-3151565/src/TestShapeList.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+public class TestShapeList {
+    private static ArrayList<Shape> shapes;
+
+    @BeforeAll
+    static void setup() {
+        shapes = new ArrayList<>();
+        // Two Circle instances
+        shapes.add(new Circle(5));
+        shapes.add(new Circle(10));
+        // Two Rhombus instances
+        shapes.add(new Rhombus(6, 4));
+        shapes.add(new Rhombus(10, 5));
+        // Two RightAngledTriangle instances
+        shapes.add(new RightAngledTriangle(5, 3, 4));
+        shapes.add(new RightAngledTriangle(10, 6, 8));
+    }
+
+    @Test
+    public void testGetAreaForAllShapes() {
+        double[] expectedAreas = {
+            Math.PI * 5 * 5,
+            Math.PI * 10 * 10,
+            6 * 4,
+            10 * 5,
+            0.5 * 3 * 4,
+            0.5 * 6 * 8
+        };
+        for (int i = 0; i < shapes.size(); i++) {
+            assertEquals(expectedAreas[i], shapes.get(i).getArea(), 0.01);
+
+        }
+    }
+
+    @Test
+    public void testGetPerimeterForAllShapes() {
+        double[] expectedPerimeters = {
+            2 * Math.PI * 5,
+            2 * Math.PI * 10,
+            4 * 6,
+            4 * 10,
+            5 + 3 + 4,
+            10 + 6 + 8
+        };
+        for (int i = 0; i < shapes.size(); i++) {
+            assertEquals(expectedPerimeters[i], shapes.get(i).getPerimeter(), 0.01);
+
+        }
+    }
+}
